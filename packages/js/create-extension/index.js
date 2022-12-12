d7818c35180cf25ea54a8ff006f7e2f7bb8322b8
Paul Sealock <psealock@gmail.com>
2022-12-12 14:36:38 +1300

    Get latest Woo version to download

--- packages/js/create-extension/index.js
+++ packages/js/create-extension/index.js
@@ -1,17 +1,39 @@
-module.exports = {
-	templatesPath: __dirname,
-	defaultValues: {
-		npmDependencies: [ '@wordpress/hooks', '@wordpress/i18n' ],
-		npmDevDependencies: [
-			'@woocommerce/dependency-extraction-webpack-plugin',
-			'@woocommerce/eslint-plugin',
-			'@wordpress/prettier-config',
-			'@wordpress/scripts',
-		],
-		namespace: 'extension',
-		license: 'GPL-3.0+',
-	},
-	customScripts: {
-		postinstall: 'composer install',
-	},
-};
+/**
+ * External dependencies
+ */
+const fetch = require( 'node-fetch' );
+
+async function getLatestWooVersion() {
+	const api =
+		'https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&request[slug]=woocommerce';
+
+	const response = await fetch( api );
+	const json = await response.json();
+
+	return json.version;
+}
+
+module.exports = ( async function () {
+	const latestWooVersion = await getLatestWooVersion();
+
+	return {
+		templatesPath: __dirname,
+		defaultValues: {
+			npmDependencies: [ '@wordpress/hooks', '@wordpress/i18n' ],
+			npmDevDependencies: [
+				'@woocommerce/dependency-extraction-webpack-plugin',
+				'@woocommerce/eslint-plugin',
+				'@wordpress/prettier-config',
+				'@wordpress/scripts',
+			],
+			namespace: 'extension',
+			license: 'GPL-3.0+',
+			attributes: {
+				latestWooVersion,
+			},
+		},
+		customScripts: {
+			postinstall: 'composer install',
+		},
+	};
+} )();
